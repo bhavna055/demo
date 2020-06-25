756010c243d32e7be03bf92f50de6cd8068b08f2
ace <bhavna.sharma.bs055@gmail.com>
2020-06-25 23:44:14 +0530

    helll0q

--- src/main/java/practice/demo/controller/EmployeeController.java
+++ src/main/java/practice/demo/controller/EmployeeController.java
@@ -7,7 +7,9 @@ import practice.demo.exception.ResourceNotFoundException;
 import practice.demo.model.employee;
 import practice.demo.repo.EmployeeRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,20 +30,49 @@ public class EmployeeController {
 //                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + departmentt));
 //        return ResponseEntity.ok().body(employee);
 //    }
+    @CrossOrigin
     @GetMapping("/employees")
     public List<employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
-
+    @CrossOrigin
     @GetMapping("/employees/{department}")
     public ResponseEntity<List<employee>> getEmployeeByDepartment(@PathVariable(value = "department") String department)
             throws ResourceNotFoundException {
         List<employee> employees = employeeRepository.findByDepartment(department);
         if (employees.isEmpty()){
+            System.out.println("in");
             throw  new ResourceNotFoundException("No employee found for department " + department);
+
         }
 
         return ResponseEntity.ok().body(employees);
+    }
+    @CrossOrigin
+    @DeleteMapping("/employees/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+    @CrossOrigin
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+                                                  @RequestBody employee employeeDetails) throws ResourceNotFoundException {
+        employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+
+        employee.setEmpName(employeeDetails.getEmpName());
+        employee.setDepartment(employeeDetails.getDepartment());
+        employee.setRole(employeeDetails.getRole());
+        final employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 
 
